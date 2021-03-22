4de3fa4091bddc0821312fa816113ffd3543337b
ImAkhil24 <56029762+ImAkhil24@users.noreply.github.com>
2021-03-22 14:16:07 +0530

    Database connection (#7)
    * added createUser route
    
    * setup the backend schmeas and controllers
    
    * did some cleanup
    
    * basic controllers of register and getuser by email set up and some basic validation

--- src/index.js
+++ src/index.js
@@ -22,6 +22,24 @@ app.use('/api', usersRoute);
 //   // have to do the login stuff.
 // });
 
+// new things
+
+// const PORT = process.env.port || 3000;
+// console.log("the server is started on port : ${PORT}");
+// console.error(error);
+// email aur password ko require krna hai
+// app.use(express.json());
+// bcrypt algorihtm for hashing
+// for authentication there is a thing callsed jwt research about it
+// npm jsonwebtoken
+// send the token in a HTTP only cookie
+// for logout just clear the cookie
+
+// private endpoints
+// some endpoints should only be accesible if the user is logged in
+// logged in means the browser have that cookie
+// cookie parser library parse the incoming cookie into a javascript object
+
 app.listen(3000, () => {
   // eslint-disable-next-line no-console
   console.log('listening to port 3000');
